69572abdb6f63ac20c713a997f40b803f5125dd1
damzG <C00313475@setu.ie>
2026-01-15 10:56:02 +0000

    Use Local Development Password

--- src/util/DBconnect.java
+++ src/util/DBconnect.java
@@ -8,8 +8,8 @@ public class DBconnect {
 
     public static final String URL =
             "jdbc:mysql://localhost:3306/hospital_db";
-    public static final String USER = "root";
-    public static final String PASSWORD = "password";
+    public static final String USER = "hms_user";
+    public static final String PASSWORD = "hms_password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
